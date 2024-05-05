805bc28a5233184ad771cf1cc96f01c83789d8f0
MrThanToeAung <thantoeaung.cu.96@gmail.com>
2024-05-05 23:40:30 +0630

    [Complete] ASP.NET Core Web Api EFCore CRUD

---
+++ TTADotNetCore.RestApi/ConnectionStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTADotNetCore.RestApi
+{
+    public static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "HP",
+            InitialCatalog = "TTADotNetCore",
+            UserID = "sa",
+            Password = "sa@123",
+            TrustServerCertificate = true
+        };
+    }
+}
+
---
+++ TTADotNetCore.RestApi/Database/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTADotNetCore.RestApi.Controllers;
+using TTADotNetCore.RestApi.Models;
+
+namespace TTADotNetCore.RestApi.Database
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+
+    }
+}
